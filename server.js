61a8113fc0c764c4182e9ee76d11b46d9617cbd9
Suman Kumar Singh <sumankumars301@gmail.com>
2026-04-18 23:18:28 +0530

    Update CORS for new Netlify URL

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: [
         'http://127.0.0.1:5500',
         'http://localhost:5500',
-        'https://studentphere0.netlify.app'
+        'https://studentsphere0.netlify.app'
     ],
     credentials: true
 }));
